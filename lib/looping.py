c66684a2531a81ba55a5f15b807840d44e90414e
ElvineJoy <elvinkuiyungest@gmail.com>
2024-07-02 16:06:22 +0300

    Looping understood

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i in range (10, 0, -1):
+        print (i)
+        i -= 1
+
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    squared_integers = [integer * integer for integer in int_list]
+    return squared_integers 
+   
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range (1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz") 
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
+happy_new_year()
+square_integers([-1,2,3,4,5,6])
